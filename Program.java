bf0ab9a7ba96e744110cbc63d4b67cd5684d8340
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2023-01-03 12:23:55 +0100

    "clean code AGAIN"

--- Program.java
+++ Program.java
@@ -11,8 +11,8 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Basket koszyk = Basket.getInstance();
-        BasketProxy koszykProxy = new BasketProxy(koszyk);
+        Basket basket = Basket.getInstance();
+        BasketProxy basketProxy = new BasketProxy(basket);
 
         System.out.println("Witamy w naszym sklepie meblowym \"Sofy i Stoły\"");
         Scanner myObj = new Scanner(System.in);
@@ -22,14 +22,14 @@ public class Program {
             System.out.println("\nPoruszaj się po programie za pomocą następujących komend:\n\tzarzadzaj - Przejdź w tryb wyboru i usuwania mebli\n\tkoszyk - sprawdź zawartość koszyka\n\tfaktura - Swtórz fakturę\n\tzamknij - Zakoncz Program");
 
             System.out.println("\nCo chcesz zrobić?");
-            String wybor = myObj.nextLine();
-            if (Objects.equals(wybor, "zarzadzaj")) {
-                new ManageBasket(koszykProxy).execute();
-            } else if (Objects.equals(wybor, "koszyk")) {
-                new ShowBasket(koszykProxy).execute();
-            } else if (Objects.equals(wybor, "faktura")) {
-                new CreateFacture(koszykProxy).execute();
-            } else if (Objects.equals(wybor, "zamknij")) {
+            String pick = myObj.nextLine();
+            if (Objects.equals(pick, "zarzadzaj")) {
+                new ManageBasket(basketProxy).execute();
+            } else if (Objects.equals(pick, "koszyk")) {
+                new ShowBasket(basketProxy).execute();
+            } else if (Objects.equals(pick, "faktura")) {
+                new CreateFacture(basketProxy).execute();
+            } else if (Objects.equals(pick, "zamknij")) {
                 program = false;
             }
         }
